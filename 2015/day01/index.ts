34062e851aa75c53643193b1a9cc5ddde0302d4a
Daniel Foust <dfoust@tutanota.com>
2023-03-18 18:43:48 -0400

    Solve part 2

--- 2015/day01/index.ts
+++ 2015/day01/index.ts
@@ -7,9 +7,16 @@ const DIRECTION_MAP: Record<string, 1 | -1> = {
 const input = loadInput(__dirname);
 
 let floor = 0;
+let firstHitBasement = null;
 
 for (let i = 0; i < input.length; i++) {
   floor += DIRECTION_MAP[input[i]];
+
+  if (floor === -1 && firstHitBasement === null) {
+    // Does not use zero offset, first position is 1
+    firstHitBasement = i + 1;
+  }
 }
 
 console.log('Final floor: ', floor);
+console.log('Position of first character to enter basement: ', firstHitBasement);
